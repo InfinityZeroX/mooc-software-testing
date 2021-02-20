b60f68ce5d1e044e8d425e070a3d0a5bd99b6d34
InfinityZeroX <InfinityZeroX@users.noreply.github.com>
2021-02-20 22:50:57 +0800

    Practical exercise #1 GHappy v2

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -3,21 +3,16 @@ package tudelft.ghappy;
 public class GHappy {
 
     public boolean gHappy(String str) {
-        assert str != null;
-
-        boolean hasG = false;
-
-        for (int i = 0; i < str.length(); ++i) {
-            if (str.charAt(i) == 'g') {
-                hasG = true;
-
+        assert str!=null;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'g') {
                 if (i > 0 && str.charAt(i-1) == 'g') { continue; }
                 if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
-
                 return false;
             }
         }
 
-        return hasG;
+        return true;
+
     }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class GHappyTest {
     @ParameterizedTest(name = "{0}: \"{1}\" = {2}")
-    @CsvSource({"Single letter, g, false", "Single letter, x, false",
-            "No G, abcde, false",
+    @CsvSource({"Single letter, g, false",
+            "No G, abcde, true",
             "Edge Gs, gg, true", "Edge Gs, ggxg, false",
             "Middle Gs, xgx, false" , "Middle Gs, xggxgx, false", "Middle Gs, xggx, true"})
     public void testAlgorithm(String partition, String input, String expectedResult)
